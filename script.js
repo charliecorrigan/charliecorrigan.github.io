ee6f6c375faf4d49a2db771799a140ad145bf16a
ccorrigan <charlie.ps.corrigan@gmail.com>
2017-04-27 12:10:57 -0600

    Remove file copies and update first blog post links

--- script.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function(){
-	$('#home_button').mouseenter(function(){
-		$('#home_button').fadeTo('fast', 1)
-	})
-		$('#home_button').mouseleave(function(){
-		$('#home_button').fadeTo('fast', 0.5)
-	})
-})
-
